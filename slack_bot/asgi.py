2dddd6fec4d13e7956b3a60db8a49dd25a2ad0ce
Oluwafemi Adenuga <phemmylintry@gmail.com>
2022-07-14 12:28:06 +0100

    fix

--- slack_bot/asgi.py
+++ slack_bot/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'slack_bot.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "slack_bot.settings")
 
 application = get_asgi_application()
--- slack_bot/urls.py
+++ slack_bot/urls.py
@@ -2,7 +2,4 @@ from django.contrib import admin
 from django.urls import path
 from events.views import Events
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('events/', Events.as_view())
-]
+urlpatterns = [path("admin/", admin.site.urls), path("events/", Events.as_view())]
